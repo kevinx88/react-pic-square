b60ec87bc4231b10de2ae7314d9d16d52bebef8d
Kevin <kevinx88@gmail.com>
2017-07-12 20:16:00 -0400

    dropzonejs canvas config

--- react-pic-square/src/assets/js/main.js
+++ react-pic-square/src/assets/js/main.js
@@ -1,48 +1,44 @@
 $(document).ready(() => {
     console.log('script is loaded!');
 
-$(function() {
-  Dropzone.options.myAwesomeDropzone = {
-    maxFilesize: 5,
-    addRemoveLinks: true,
-    dictResponseError: "Server not Configured",
-    acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
-    init: function() {
-      var self = this;
-      // config
-      self.options.addRemoveLinks = true;
-      self.options.dictRemoveFile = "Delete";
-      //New file added
-      self.on("addedfile", function(file) {
-        console.log("new file added ", file);
-      });
-      // Send file starts
-      self.on("sending", function(file) {
-        console.log("upload started", file);
-        $(".meter").show();
-      });
+    $(function() {
+      Dropzone.options = {
+        maxFilesize: 5,
+        acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
+        init: function() {
+          // config
+          this.options.dictRemoveFile = "Delete";
+          //New file added
+          this.on("addedfile", function(file) {
+            console.log("new file added ", file);
+          });
+          // Send file starts
+          this.on("sending", function(file) {
+            console.log("upload started", file);
+            $(".meter").show();
+          });
 
-      // File upload Progress
-      self.on("totaluploadprogress", function(progress) {
-        console.log("progress ", progress);
-        $(".roller").width(progress + "%");
-      });
+          // File upload Progress
+          this.on("totaluploadprogress", function(progress) {
+            console.log("progress ", progress);
+            $(".roller").width(progress + "%");
+          });
 
-      self.on("queuecomplete", function(progress) {
-        $(".meter").delay(999).slideUp(999);
-      });
+          this.on("queuecomplete", function(progress) {
+            $(".meter").delay(999).slideUp(999);
+          });
 
-      // On removing file
-      self.on("removedfile", function(file) {
-        console.log(file);
-      });
-    }
-  };
-});
+          // On removing file
+          this.on("removedfile", function(file) {
+            console.log(file);
+          });
+        }
+      };
+    });
 
     $(function() {
         $("#btnSave").click(function() {
-            html2canvas($("#picSquare"), {
+            html2canvas($(".square-container"), {
                 onrendered: function(canvas) {
                     document.body.appendChild(canvas);
 
@@ -54,5 +50,6 @@ $(function() {
             });
         });
     });
+
 
 });
--- react-pic-square/src/components/PicSquare.js
+++ react-pic-square/src/components/PicSquare.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 import request from "superagent";
 
-const CLOUDINARY_UPLOAD_PRESET = 'bmzjbxoq';
-const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/react-cloudinary/upload';
 
 class PicSquare extends Component {
     constructor(props) {
